7b83a03ed8881ce1b5441b2c70edfc9c7664a781
Riad Bensalem <fr_bensalem@esi.dz>
2018-09-04 21:22:41 +0200

    completer le formulaire fuite et radio group

--- app/src/main/java/com/example/ryad/ggg/fuite_form.java
+++ app/src/main/java/com/example/ryad/ggg/fuite_form.java
@@ -6,13 +6,23 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class fuite_form extends AppCompatActivity {
 
+    RadioGroup rg1,rg2;
+    RadioButton rb;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fuite_form);
+        rg1 = (RadioGroup) findViewById(R.id.rg1);
+        rg2 = (RadioGroup) findViewById(R.id.rg2);
+    tv=(TextView)findViewById(R.id.name);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,9 +34,16 @@ public class fuite_form extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     public void sendClick(View view) {
+        int rbId = rg1.getCheckedRadioButtonId();
+        int rbId2=rg2.getCheckedRadioButtonId();
+        rb=(RadioButton) findViewById(rbId);
+        Toast.makeText(this, rb.getText(), Toast.LENGTH_SHORT).show();
+        tv.setText(rb.getText());
+
 
     }
 }
